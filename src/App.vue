e70fbf1858cff35d29dd708ce144b03a4d1fa822
StefanDellamore <dellamoreste.info@gmail.com>
2024-03-14 17:26:06 +0100

    Layout

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return{
+            message: 'hello'
+        }
+    }
+};
+</script>
+
+<template>
+    
+    <header>
+        header
+    </header>
+
+    <main>
+        {{ message }}
+    </main>
+
+    <footer>
+        footer
+    </footer>
+
+</template>
+
+<style scoped>
+
+</style>
