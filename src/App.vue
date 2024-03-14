187002b65fb8d2c785cabb478f761fb52fe2cb3f
StefanDellamore <dellamoreste.info@gmail.com>
2024-03-14 17:42:47 +0100

    Import component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <script>
+
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
 export default {
     data() {
         return{
             message: 'hello'
         }
+    },
+    components: {
+        HeaderComponent,
+        MainComponent,
+        FooterComponent
     }
 };
 </script>
 
 <template>
     
-    <header>
-        header
-    </header>
+    <HeaderComponent />
 
-    <main>
-        {{ message }}
-    </main>
+    <MainComponent />
 
-    <footer>
-        footer
-    </footer>
+    <FooterComponent />
 
 </template>
 
